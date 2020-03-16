f1be57dd3c86d7360a05ab2d705434bf5c37d810
jtzanetatos <tzanetatosi@gmail.com>
2020-03-16 11:00:52 +0200

    Update py_main.py

--- src/python/py_main.py
+++ src/python/py_main.py
@@ -9,11 +9,29 @@ Created on Fri Feb 28 13:14:01 2020
 
 import cv2 as cv
 import numpy as np
+from scipy.stats import mode
+from skimage.measure import label
 import wx
 # TODO: Implement GTK+3 module; implement OS check; implement cupy for efficiency.
-# Implement NumPy's array iteration
+# Implement NumPy's array iteration.
 
 def get_path(wildcard):
+    '''
+    
+
+    Parameters
+    ----------
+    wildcard : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    path : TYPE
+        DESCRIPTION.
+
+    '''
+    
+    
     app = wx.App(None)
     style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
     dialog = wx.FileDialog(None, 'Open', wildcard=wildcard, style=style)
@@ -45,7 +63,7 @@ def imBackSup(fr_bw, bg_bw, threshold):
     '''
     
     # Evaluate in/out frames dimensions
-    height, width = bg_bw.shape()
+    height, width = bg_bw.shape
     
     # Evaluate abs difference between background model & current frame
     fr_diff = np.abs((np.uint32(fr_bw) - np.uint32(bg_bw)))
@@ -56,21 +74,22 @@ def imBackSup(fr_bw, bg_bw, threshold):
     # Loop through input frame's elements & implement threshold filtering
     # (High pass theshold filtering)
     # TODO: implement NumPy approach on array iteration
-    for i in range(width):
-        for k in range(height):
-            # Current element belongs in foreground
-            if (fr_diff[i, k] > threshold):
-                fg[i, k] = fr_bw[i, k]
-            # Element belongs in background
-            else:
-                fg[i, k] = np.uint8(0)
+    fg = np.where(fr_diff > threshold, np.uint8(fr_bw), np.uint8(0))
+    # for i in range(width):
+    #     for k in range(height):
+    #         # Current element belongs in foreground
+    #         if (fr_diff[i, k] > threshold):
+    #             fg[i, k] = fr_bw[i, k]
+    #         # Element belongs in background
+    #         else:
+    #             fg[i, k] = np.uint8(0)
     # Set current frame as background model for next iteration
     bg_bw = fr_bw
     
     # Return foreground & background models
-    return (np.array((fr_bw, bg_bw)))
+    return (np.array((fg, bg_bw)))
 
-def contour(fg, ns1):
+def contour(fg, ns1, nsNN):
     '''
     
 
@@ -89,36 +108,52 @@ def contour(fg, ns1):
     '''
     
     # Apply median filtering on foregound frame
-    fg = cv.medianBlur(fg, ksize=7)
+    fg = cv.medianBlur(fg, ksize=nsNN)
     
     # Create disk structuring element
-    st = cv.getStructuringElement(cv.MORPH_ELLIPSE, ns1)
+    st = cv.getStructuringElement(cv.MORPH_ELLIPSE, ksize=(ns1, ns1))
     
     # Apply morphological closing on frame; MATLAB's 'fill' operation omitted.
-    fgc = cv.MorphologyEx(fg, cv.MORPH_CLOSE, element=st, iterations=1)
+    fgc = cv.morphologyEx(fg, cv.MORPH_CLOSE, kernel=st, iterations=1)
     
     # Return captured contour
     return fgc
 
-def imlabel(fgc, conncomp, minsizeofCC):
+# def imlabel(fgc, conncomp, minsizeofCC):
     
     
-    # Counter for num of objects in scene
-    objCounter = 0
+#     # Counter for num of objects in scene
+#     objCounter = 0
     
     
 
-def imcluster(num, fgc):
+# def imcluster(num, fgc):
+#     #TODO: Further develop function.
     
     
+#     # Mean value
+#     meanvar = np.mean(allAeras)
     
+#     # Most frequent value
+#     freqvar = mode(allAeras)
+    
+#     # Evaluate distance from mean value
+#     distance = (allAeras - meanvar) / (num - 1)
+    
+#     # Loop through each cluster
+#     for i in range(num):
+#         thisCentroid = 
+#         # Current cluster greater than frequent area value
+#         if distance[i] > freqvar:
+#             clusterTemp[i] = thisCentroid
+            
 
 def main():
     
     
     # Select video file; at the time, .avi & .mp4 files supported.
     #TODO: implement appropriate wildcard argument
-    vid_file = get_path('*.avi', '*.mp4')
+    vid_file = get_path('*.avi')
     
     # Create video reader object
     vidObj = cv.VideoCapture(vid_file)
@@ -132,21 +167,55 @@ def main():
     # Background pixel value threshold
     thresh = 25
     
+    # Structure disk radius
+    ns1 = 15
+    
+    # Kernel size/neighborhood windows for median filter
+    nsNN = 7
+    
+    # Initialize RGB output frame
+    fgcRGB = np.zeros_like(bg, dtype=np.uint8)
+    
+    # Frame counter
+    cntr = 0
+    
     while ret:
         # Read current frame
         ret, fr = vidObj.read()
         
+        # Break at end of video stream
+        if ret == False:
+            break
+        
         # Convert current frame to greyscale
-        fr_bw = cv.cvtColor(fr_bw, cv.COLOR_BGR2GRAY)
+        fr_bw = cv.cvtColor(fr, cv.COLOR_BGR2GRAY)
         
         # Background subtraction
         fg, bg_bw = imBackSup(fr_bw, bg_bw, threshold=thresh)
         
         # Contour extraction
-        fgc = contour(fg, ns1)
+        fgc = contour(fg, ns1, nsNN)
         
         # Object labeling
-        imlabel(fgc, conncomp, minsizeofCC)
+        # imlabel(fgc, conncomp, minsizeofCC)
+        
+        # Shadow removal & crowd analysis function
+        # cluster = imcluster()
+        
+        # Convert Grayscale frame to RGB
+        fgcRGB[:, :, 0] = np.uint8(fgc) * np.uint8(fr[:, :, 0])
+        fgcRGB[:, :, 1] = np.uint8(fgc) * np.uint8(fr[:, :, 1])
+        fgcRGB[:, :, 2] = np.uint8(fgc) * np.uint8(fr[:, :, 2])
+        
+        # Increment counter
+        cntr += 1
+        
+        # Show current frame
+        cv.imshow('Frame', fgcRGB)
+        
+    # Release capture & close all windows
+    vidObj.release()
+    cv.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
