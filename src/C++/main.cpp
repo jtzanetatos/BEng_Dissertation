1c140084c8a0c9e8b1fbbbed8f53a0aef1e37028
jtzanetatos <58182829+jtzanetatos@users.noreply.github.com>
2020-08-06 13:36:42 +0300

    Update main.cpp
    Frame iteration functions need further work.

--- src/C++/main.cpp
+++ src/C++/main.cpp
@@ -7,37 +7,67 @@
 using namespace std;
 
 // pair <cv::Mat, cv::Mat> imBackSub(cv::Mat bg, cv::Mat fr, int thresh) {
-//   // Initialize output par
-//   pair <cv::Mat, cv::Mat> out;
-//   // Initialize background subtraction frame
-//   cv::Mat fr_diff, bg_bw, fr_bw, fg;
+//         // Initialize output par
+//         pair <cv::Mat, cv::Mat> out;
+//         // Initialize background subtraction frame
+//         cv::Mat fr_diff, bg_bw, fr_bw, fg;
 //
 //
-//   // Convert input frames into Grayscale
-//   cv::cvtColor(fr, fr_bw, cv::COLOR_BGR2GRAY);
-//   cv::cvtColor(bg, bg_bw, cv::COLOR_BGR2GRAY);
+//         // Convert input frames into Grayscale
+//         cv::cvtColor(fr, fr_bw, cv::COLOR_BGR2GRAY);
+//         cv::cvtColor(bg, bg_bw, cv::COLOR_BGR2GRAY);
 //
-//   // Subtract background from foreground
-//   fr_diff = fr_bw - bg_bw;
+//         // Subtract background from foreground
+//         cv::absdiff(fr_bw, bg_bw, fr_diff);
 //
-//   // Loop through Subtracted frame model & apply high pass filtering
-//   for(int i=0; i < fr.cols; i++) {
-//     for(int j=0; j < fr.rows; j++) {
-//       if (fr_diff[i][j] > thresh)
-//           fg[i][j] = fr_bw[i][j];
-//       else
-//           fg[i][j] = 0;
-//     }
-//   }
+//         // Assert only char type images
+//         CV_Assert(fg.depth() == CV_8UC3);
 //
-//   // Update background frame for next iteration
-//   bg_bw = fr_bw;
+//         int channels = fg.channels();
 //
-//   // Set output pairs
-//   out.first = fg;
-//   out.second = bg_bw;
+//         int nRows = fg.rows;
+//         int nCols = fg.cols * channels;
 //
-//   return out;
+//         if ( fg.isContinuous() ) {
+//                 nCols *= nRows;
+//                 nRows = 1;
+//         }
+//
+//         int i, j;
+//         uchar *pin, *pout;
+//
+//         for( i = 0; i < nRows; ++i) {
+//
+//                 pin = fr_diff.ptr<uchar>(i);
+//                 pout = fg.ptr<uchar>(i);
+//                 for( j = 0; i < nCols; ++j) {
+//                         if( fr_diff[pin[j]] > thresh ) {
+//                                 pout[j] = fr_diff[pout[j]];
+//
+//                         }
+//                         else {
+//                                 pout[j] = 0;
+//                         }
+//                 }
+//         }
+//         // Loop through Subtracted frame model & apply high pass filtering
+//         // for(int i=0; i < fr.cols; i++) {
+//         //         for(int j=0; j < fr.rows; j++) {
+//         //                 if (fr_diff[i][j] > thresh)
+//         //                         fg.at<uchar>(i, j) = fr_bw.at<uchar>(i, j);
+//         //                 else
+//         //                         fg.at<uchar>(i, j) = 0;
+//         //         }
+//         // }
+//
+//         // Update background frame for next iteration
+//         bg_bw = fr_bw;
+//
+//         // Set output pairs
+//         out.first = fg;
+//         out.second = bg_bw;
+//
+//         return out;
 // }
 
 cv::Mat contour(cv::Mat fg, int ns1, int nn) {
@@ -58,7 +88,33 @@ cv::Mat contour(cv::Mat fg, int ns1, int nn) {
         return fgc;
 }
 
+cv::Mat clrCnv(cv::Mat fr, cv::Mat fgc) {
 
+        // Assert input frame's dtype
+        CV_Assert(fr.depth() == CV_8U);
+
+        int channels = fr.channels();
+        int nRows = fr.rows;
+        int nCols = fr.cols * channels;
+
+        if( fr.isContinuous() ) {
+                nCols *= nRows;
+                nRows = 1;
+        }
+
+        // Convert RGB/BGR array into vector & split colour channels
+        std::vector<cv::Mat> ch_split;
+
+        cv::split(fr, ch_split);
+
+        cv::Mat fgcRGB = cv::Mat( fr.rows, fr.cols, CV_8UC3, CV_RGB(1,1,1) );
+
+        uchar* p;
+        for (int i = 0; i < fr.channels(); i++) {
+
+        }
+
+}
 
 int main(int argc, char **argv)
 {
@@ -77,10 +133,11 @@ int main(int argc, char **argv)
         }
 
         // Initialize background & current frame
-        cv::Mat bg, fr;
+        cv::Mat bg, fr, fgcRGB;
 
         // Read first frame as Background
         cap >> bg;
+
 
         // Loop through video stream/file
         while(1) {
@@ -93,6 +150,10 @@ int main(int argc, char **argv)
                         printf("End of video file/stream.\n");
                         break;
                 }
+
+
+                // Convert frame from Grayscale to RGB/BGR
+                // cv::mutliply(fr, fgc, fgcRGB);
 
                 // Show input frame
                 cv::imshow("Frame", fr);
